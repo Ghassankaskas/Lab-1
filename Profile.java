aaa9bd88919dd39dbca70be5730d2fa1ab56a0f6
Ghassankaskas <ghassan.kaskas@lau.edu>
2023-01-26 21:05:14 +0200

    Hobby added

--- Profile.java
+++ Profile.java
@@ -2,7 +2,7 @@ public class Profile {
 	public static void main(String[] args){
 		System.out.println("Ghassan Kaskas");
 		System.out.println("Mechatronics Engineering");
-		System.out.print("1st year");
-		
+		System.out.println("1st year");
+		System.out.print( "Basketball"):
 	}
 }
